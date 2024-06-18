afcb8607189c45d1f7ef98884d2f51e8d7753ec4
lucas-de-lima <lucasramosdelima@gmail.com>
2024-06-18 09:04:08 -0300

    feat: OrderDAOImpl

---
+++ app/src/main/java/com/example/mvvmcoffee/data/database/OrderDAOImpl.kt
@@ -0,0 +1,25 @@
+package com.example.mvvmcoffee.data.database
+
+import com.example.mvvmcoffee.data.dto.OrderDTO
+
+class OrderDAOImpl : OrderDAO {
+    override fun save(entity: OrderDTO) {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(entity: OrderDTO) {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun findAll(): Array<OrderDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun findById(id: Int): OrderDTO {
+        TODO("Not yet implemented")
+    }
+}
