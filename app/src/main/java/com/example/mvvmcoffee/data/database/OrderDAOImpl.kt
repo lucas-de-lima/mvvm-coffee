70c186091642f2e2672e6380debb94859c7517c7
lucas-de-lima <lucasramosdelima@gmail.com>
2024-06-18 11:37:13 -0300

    feat: save method for save (or create) a new Order

--- app/src/main/java/com/example/mvvmcoffee/data/database/OrderDAOImpl.kt
+++ app/src/main/java/com/example/mvvmcoffee/data/database/OrderDAOImpl.kt
@@ -1,10 +1,23 @@
 package com.example.mvvmcoffee.data.database
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.example.mvvmcoffee.data.dto.OrderDTO
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class OrderDAOImpl : OrderDAO {
-    override fun save(entity: OrderDTO) {
-        TODO("Not yet implemented")
+    private val db = Firebase.firestore
+
+    override fun save(order: OrderDTO) {
+        db.collection("orders")
+            .add(order)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG,"Document added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG,"Error adding document: $e")
+            }
     }
 
     override fun update(entity: OrderDTO) {
