4d9e52984297dc2f995d440db5ffbe81f35cd370
lucas-de-lima <lucasramosdelima@gmail.com>
2024-06-18 09:03:31 -0300

    feat: OrderDTO

---
+++ app/src/main/java/com/example/mvvmcoffee/data/dto/OrderDTO.kt
@@ -0,0 +1,41 @@
+package com.example.mvvmcoffee.data.dto
+
+import com.example.mvvmcoffee.enums.OrderStatusEnum
+import com.example.mvvmcoffee.enums.PaymentStatusEnum
+import java.util.Date
+
+class OrderDTO(
+    private val id: Int,
+    private val customerName: String,
+    private val orderItems: List<OrderItemDTO>,
+    private val totalAmount: Double,
+    private val paymentStatus: PaymentStatusEnum,
+    private val specialInstructions: String,
+    private val orderDate: Date,
+    private val orderStatus: OrderStatusEnum
+) {
+    val getId: Int
+        get() = id
+
+    val getCustomerName: String
+        get() = customerName
+
+    val getOrderItems: List<OrderItemDTO>
+        get() = orderItems
+
+    val getTotalAmount: Double
+        get() = totalAmount
+
+    val getPaymentStatus: PaymentStatusEnum
+        get() = paymentStatus
+
+    val getSpecialInstructions: String
+        get() = specialInstructions
+
+    val getOrderDate: Date
+        get() = orderDate
+
+    val getOrderStatus: OrderStatusEnum
+        get() = orderStatus
+
+}
