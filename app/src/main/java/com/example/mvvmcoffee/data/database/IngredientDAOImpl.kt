5e84d633fbe8d924430d780bbddbbebfa1c99730
lucas-de-lima <lucasramosdelima@gmail.com>
2024-06-19 14:52:49 -0300

    feat: save method for save ingredients

--- app/src/main/java/com/example/mvvmcoffee/data/database/IngredientDAOImpl.kt
+++ app/src/main/java/com/example/mvvmcoffee/data/database/IngredientDAOImpl.kt
@@ -1,10 +1,23 @@
 package com.example.mvvmcoffee.data.database
 
+import android.util.Log
 import com.example.mvvmcoffee.data.dto.IngredientDTO
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class IngredientDAOImpl : IngredientDAO {
+    private val db = Firebase.firestore
+
     override fun save(entity: IngredientDTO) {
-        TODO("Not yet implemented")
+        db.collection("ingredients")
+            .document(entity.id!!)
+            .set(entity)
+            .addOnSuccessListener { documentReference ->
+                Log.d("IngredientDAOImpl", "Document added with ID: ${entity.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("IngredientDAOImpl", "Error adding document: $e")
+            }
     }
 
     override fun update(entity: IngredientDTO) {
