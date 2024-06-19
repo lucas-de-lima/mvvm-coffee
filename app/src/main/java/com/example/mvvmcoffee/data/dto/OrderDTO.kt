9ff1a81438ee1834cfb19cc8de4c3018ae2bb69c
lucas-de-lima <lucasramosdelima@gmail.com>
2024-06-19 13:54:05 -0300

    refactor: OrderDTO

--- app/src/main/java/com/example/mvvmcoffee/data/dto/OrderDTO.kt
+++ app/src/main/java/com/example/mvvmcoffee/data/dto/OrderDTO.kt
@@ -5,60 +5,13 @@ import com.example.mvvmcoffee.enums.PaymentStatusEnum
 import java.util.Date
 
 data class OrderDTO(
-    private var _id: String? = null,
-    private var _customerName: String? = null,
-    private var _orderItems: List<OrderItemDTO>? = null,
-    private var _totalAmount: Double? = null,
-    private var _paymentStatus: PaymentStatusEnum? = null,
-    private var _specialInstructions: String? = null,
-    private var _orderDate: Date? = null,
-    private var _orderStatus: OrderStatusEnum? = null
+    var id: String? = null,
+    var customerName: String? = null,
+    var orderItems: List<OrderItemDTO>? = null,
+    var totalAmount: Double? = null,
+    var paymentStatus: PaymentStatusEnum? = null,
+    var specialInstructions: String? = null,
+    var orderDate: Date? = null,
+    var orderStatus: OrderStatusEnum? = null
 ) {
-    var id: String?
-        get() = _id
-        set(value) {
-            _id = value
-        }
-
-    var customerName: String?
-        get() = _customerName
-        set(value) {
-            _customerName = value
-        }
-
-    var orderItems: List<OrderItemDTO>?
-        get() = _orderItems
-        set(value) {
-            _orderItems = value
-        }
-
-    var totalAmount: Double?
-        get() = _totalAmount
-        set(value) {
-            _totalAmount = value
-        }
-
-    var paymentStatus: PaymentStatusEnum?
-        get() = _paymentStatus
-        set(value) {
-            _paymentStatus = value
-        }
-
-    var specialInstructions: String?
-        get() = _specialInstructions
-        set(value) {
-            _specialInstructions = value
-        }
-
-    var orderDate: Date?
-        get() = _orderDate
-        set(value) {
-            _orderDate = value
-        }
-
-    var orderStatus: OrderStatusEnum?
-        get() = _orderStatus
-        set(value) {
-            _orderStatus = value
-        }
 }
