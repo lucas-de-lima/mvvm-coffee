68ffaec99f63ee409d4e3a19b4606ba6bbb85bd3
lucas-de-lima <lucasramosdelima@gmail.com>
2024-06-19 14:53:16 -0300

    refactor: update the ingredientDTO

--- app/src/main/java/com/example/mvvmcoffee/data/dto/IngredientDTO.kt
+++ app/src/main/java/com/example/mvvmcoffee/data/dto/IngredientDTO.kt
@@ -1,9 +1,9 @@
 package com.example.mvvmcoffee.data.dto
 
 data class IngredientDTO(
-    private val id: Long? = null,
-    private val name: String? = null,
-    private val quantity: Int? = null, // Quantidade em estoque
-    private val unit: String? = null // Unidade de medida, como "grams", "ml", etc.
+    var id: String? = null,
+    var name: String? = null,
+    var quantity: Int? = null, // Quantidade em estoque
+    var unit: String? = null // Unidade de medida, como "grams", "ml", etc.
 ) {
 }
