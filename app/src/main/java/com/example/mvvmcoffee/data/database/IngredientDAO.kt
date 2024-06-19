07b55c4fe0150821b8380c7f89862dff021c6441
lucas-de-lima <lucasramosdelima@gmail.com>
2024-06-19 14:31:17 -0300

    feat: ingredients DAO and impl

---
+++ app/src/main/java/com/example/mvvmcoffee/data/database/IngredientDAO.kt
@@ -0,0 +1,6 @@
+package com.example.mvvmcoffee.data.database
+
+import com.example.mvvmcoffee.data.dto.IngredientDTO
+
+interface IngredientDAO : DAO<IngredientDTO> {
+}
---
+++ app/src/main/java/com/example/mvvmcoffee/data/database/IngredientDAOImpl.kt
@@ -0,0 +1,25 @@
+package com.example.mvvmcoffee.data.database
+
+import com.example.mvvmcoffee.data.dto.IngredientDTO
+
+class IngredientDAOImpl : IngredientDAO {
+    override fun save(entity: IngredientDTO) {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(entity: IngredientDTO) {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun findAll(): Array<IngredientDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun findById(id: String): Result<IngredientDTO> {
+        TODO("Not yet implemented")
+    }
+}
