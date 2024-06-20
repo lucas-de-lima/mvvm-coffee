d1a2c070acdef1393dc2dbb454e77b1fc5fda744
lucas-de-lima <lucasramosdelima@gmail.com>
2024-06-20 13:59:06 -0300

    refactor: update DAO in findAll method to "Result<List<T>>""

--- app/src/main/java/com/example/mvvmcoffee/data/database/DAO.kt
+++ app/src/main/java/com/example/mvvmcoffee/data/database/DAO.kt
@@ -4,6 +4,6 @@ interface DAO<T> {
     fun save(entity: T): Unit;
     fun update(entity: T): Unit;
     fun delete(id: String): Unit;
-    suspend fun findAll(): Array<T>;
+    suspend fun findAll(): Result<List<T>>;
     suspend fun findById(id: String): Result<T>;
 }
